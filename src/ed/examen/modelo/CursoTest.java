ca390e590a0993927e13f33a12e024241cddc560
Martin Aguado <themartinex98@gmail.com>
2020-05-06 21:08:21 +0200

    implementacion test <EliminarAlumno>

--- src/ed/examen/modelo/CursoTest.java
+++ src/ed/examen/modelo/CursoTest.java
@@ -6,9 +6,18 @@ import org.junit.jupiter.api.Test;
 
 class CursoTest {
 
+	Persona p = new Persona("18079115B", "Martin", "Aguado");
+    Curso c = new Curso();
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testEliminarAlumno() {
+		try {
+			p.setDni("18079115B");
+			assertEquals("18079115B", p.getDni());
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 	}
 
 }
