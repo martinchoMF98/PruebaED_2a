12c05f320e2aed2219d01a0a8ed0b62debe37a8a
Martin Aguado <themartinex98@gmail.com>
2020-05-06 20:55:42 +0200

    CursoTest y PersonaTest

---
+++ src/ed/examen/modelo/CursoTest.java
@@ -0,0 +1,14 @@
+package ed.examen.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CursoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
