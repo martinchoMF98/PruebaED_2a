ea6efd46eae58266e053251ed30900fa67c6316c
Martin Aguado <themartinex98@gmail.com>
2020-05-06 21:54:37 +0200

    implementacion test <Persona>

--- src/ed/examen/modelo/PersonaTest.java
+++ src/ed/examen/modelo/PersonaTest.java
@@ -1,14 +1,16 @@
 package ed.examen.modelo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class PersonaTest {
 
+	Persona p = new Persona("18079115B", "Martin", "Aguado");
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testPersonaStringStringString() {
+		assertEquals("18079115B", "Martin", "Aguado");
 	}
 
 }
